ee2f98e671755df6859ac80aa5a8aec9b2c8bd88
HXwesleyXH <HXwesleyXH@hotmail.com>
2024-09-01 18:59:03 -0300

    add run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ chmod +x ./hello-server.go
 
 sleep 5
 
-for LOGIN in Homer Bart Maggie;
-do
-echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
+for LOGIN in Homer Bart Maggie; do
+    echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
 done
